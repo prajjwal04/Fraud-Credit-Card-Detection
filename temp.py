56cd1cbd85b0fc83e89e37142cf394bc8c2586cf
prajjwal04 <prajjwal.agarwal992@gmail.com>
2020-02-18 00:22:32 +0530

    Project Completed

--- temp.py
+++ temp.py
@@ -39,3 +39,64 @@ corrmat = data.corr()
 fig = plt.figure(figsize = (12,9))
 sns.heatmap(corrmat, vmax = .8 , square = True)
 plt.show()
+
+#Get all the columns from the Dataframe
+columns = data.columns.tolist()
+
+#Filter the columns to remove data we do not want
+columns = {c for c in columns if c not in ["Class"]}
+
+#Store the variables we'll be predicting on
+target = "Class"
+
+X = data[columns]
+Y = data[target]
+
+#Predict the shapes of X and Y
+print(X.shape)
+print(Y.shape)
+
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+
+#define random state
+state = 1
+
+#defining outlier detection methods
+
+classifiers = {"Isolation Forest" : IsolationForest(max_samples=len(X),
+                                                   contamination = outlier_fraction,
+                                                   random_state = state), 
+"Local Outlier Factor" : LocalOutlierFactor(n_neighbors = 20, contamination = outlier_fraction)}
+
+#Fitiing the model 
+n_outliers = len(Fraud)
+
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    
+    #fit the data and tag outliers
+    if clf_name == "Local Outlier Factor":
+        y_pred = clf.fit_predict(X)
+        scores_pred = clf.negative_outlier_factor_
+    else:
+        clf.fit(X)
+        scores_pred = clf.decision_function(X)
+        y_pred = clf.predict(X)
+        
+        
+    #Reshape the prediction value to 0 for valid, 1 for fraud
+    y_pred[y_pred == 1] = 0
+    y_pred[y_pred == -1] = 1
+    
+    n_errors = (y_pred != Y).sum()
+    
+    #Run classification metrics
+    print('{}: {}'.format(clf_name, n_errors))
+    print(accuracy_score(Y, y_pred))
+    print(classification_report(Y, y_pred))
+    
+    
+    
+    
+    
